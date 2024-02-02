273ca065169bedee9919182e22f1fd6872509cf9
Bearcat <bwigal2@nastudents.org>
2024-02-01 22:41:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // Gui made by Blake!
 
-document.body.style.backgroundColor = "blue";
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var rectWidth = 200;
+var rectHeight = 150;
+var rectX = (canvas.width - rectWidth) / 2;
+var rectY = (canvas.height - rectHeight) / 2;
+
+ctx.fillStyle = "blue";
+ctx.fillRect(rectX, rectY, rectWidth, rectHeight);
+
